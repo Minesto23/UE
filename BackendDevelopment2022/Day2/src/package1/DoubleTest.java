c3577cec30730ad526bd9743e79ec097753f49e9
Miguel Morales <minesto23@gmail.com>
2022-04-06 06:05:26 -0400

    second class before break time

---
+++ BackendDevelopment2022/Day2/src/package1/DoubleTest.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = 16.9;
+		double b = 7.8;
+		double c;
+		
+		c = a % b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ BackendDevelopment2022/Day2/src/package1/IntTest.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class IntTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a, b, c;
+		
+		a = 52;
+		b = 7;
+		c = a % b;
+		
+		System.out.println("The remain of 52 / 7: " + c);
+	}
+
+}
