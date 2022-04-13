faaac3585a96b60c25703984b09e32625b2386b3
Miguel Morales <minesto23@gmail.com>
2022-04-13 05:22:07 -0400

    parsetest

---
+++ BackendDevelopment2022/Day2/src/package1/ParseTest.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class ParseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "125";
+		int a;
+		
+		a = Integer.parseInt(str);
+		System.out.println(a);
+
+	}
+
+}
