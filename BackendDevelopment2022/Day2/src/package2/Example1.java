6d0d87e01ad710e714a78a17f268ee5f098e3ce2
Miguel Morales <minesto23@gmail.com>
2022-04-13 06:43:52 -0400

    example if statement

---
+++ BackendDevelopment2022/Day2/src/package2/Example1.java
@@ -0,0 +1,20 @@
+package package2;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x;
+		
+		x = -5;
+		
+		if (x <0 ) {
+			x = -x;
+		}
+		
+		System.out.println(x);
+
+	}
+
+}
---
+++ BackendDevelopment2022/Day2/src/package2/IfTest.java
@@ -0,0 +1,22 @@
+package package2;
+
+public class IfTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		
+		a = 9;
+		b = 10;
+		c = a + b;
+		
+		if (c<=20) {
+			System.out.println(c);
+		}
+		
+		
+
+	}
+
+}
